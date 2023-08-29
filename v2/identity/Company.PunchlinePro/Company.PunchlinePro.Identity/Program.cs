c2be5cd037b9cbfe76f577667fe0899a3facb84f
gerkai <gerkai202@gmail.com>
2023-08-29 18:55:39 -0500

    Update Program.cs

--- v2/identity/Company.PunchlinePro/Company.PunchlinePro.Identity/Program.cs
+++ v2/identity/Company.PunchlinePro/Company.PunchlinePro.Identity/Program.cs
@@ -68,7 +68,7 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || true)
 {
     app.UseSwagger();
     app.UseSwaggerUI();
