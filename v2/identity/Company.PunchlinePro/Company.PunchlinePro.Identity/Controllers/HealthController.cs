c47c6d7d473a94d435c3eef3be55f967d2a5c7c4
gerkai <gerkai202@gmail.com>
2023-09-05 19:58:55 -0500

    Added Health Check

---
+++ v2/identity/Company.PunchlinePro/Company.PunchlinePro.Identity/Controllers/HealthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company.PunchlinePro.Identity.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+
+    }
+}
